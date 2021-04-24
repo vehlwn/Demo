41e8868a07765121b5b2cef58534cd0d9cf13489
Ivan Bogdanov <c3t3o7@gmail.com>
2021-04-24 15:29:59 +0400

    Add mapping variable

--- backup.sh
+++ backup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+readonly SOURCE_DIR="/mnt/e/1/0 /home/vehlwn"
+readonly LUKS_DEVICE="/dev/sda3"
+readonly MAPPING_NAME="cryptbackup"
+
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -7,23 +12,22 @@ function cleanup()
 {
     if [ "${SHOULD_UMOUNT}" == "1" ]; then
         echo "Umounting..."
-        umount /mnt/cryptbackup
-        cryptsetup close cryptbackup
+        umount "/mnt/${MAPPING_NAME}"
+        cryptsetup close "${MAPPING_NAME}"
     fi
 }
 trap cleanup EXIT
 
 SHOULD_UMOUNT="0"
-if [ ! -e /dev/mapper/cryptbackup ]; then
+if [ ! -e "/dev/mapper/${MAPPING_NAME}" ]; then
     echo "Mounting..."
-    cryptsetup open /dev/sda3 cryptbackup
-    mount /dev/mapper/cryptbackup /mnt/cryptbackup
+    cryptsetup open "${LUKS_DEVICE}" "${MAPPING_NAME}"
+    mkdir --parents "/mnt/${MAPPING_NAME}"
+    mount "/dev/mapper/${MAPPING_NAME}" "/mnt/${MAPPING_NAME}"
     SHOULD_UMOUNT="1"
 fi
 
-readonly SOURCE_DIR="/mnt/e/1/0 /home/vehlwn"
-readonly BACKUP_DIR="/mnt/cryptbackup/backup"
-
+readonly BACKUP_DIR="/mnt/${MAPPING_NAME}/backup"
 readonly OPTS="--archive --acls --xattrs --human-readable --partial --one-file-system --delete --progress --verbose"
 readonly DATETIME="$(date '+%Y-%m-%dT%H:%M:%S')"
 readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
