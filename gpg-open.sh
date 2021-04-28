b60cba0d765358f37e69747b3462c8096b653d18
Ivan Bogdanov <c3t3o7@gmail.com>
2021-04-28 18:45:26 +0400

    Fix braces

--- gpg-open.sh
+++ gpg-open.sh
@@ -13,7 +13,7 @@ set -o nounset
 function cleanup()
 {
     echo "Deleting ${OUT_DIR}..."
-    rm -rf "$OUT_DIR"
+    rm -rf "${OUT_DIR}"
 }
 trap cleanup EXIT
 
@@ -21,15 +21,15 @@ readonly IN_FILE="$1"
 readonly OUT_DIR="$(mktemp --directory)"
 readonly OUT_FILE="${OUT_DIR}/$(basename "${IN_FILE}" | sed 's/\.gpg$//')"
 
-gpg --decrypt --yes --output "$OUT_FILE" "$IN_FILE"
+gpg --decrypt --yes --output "${OUT_FILE}" "${IN_FILE}"
 readonly OLD_MODIFY="$(stat --format="%Y" "${OUT_FILE}")"
 echo "Opening ${OUT_FILE}..."
-nvim "$OUT_FILE"
+nvim "${OUT_FILE}"
 readonly NEW_MODIFY="$(stat --format="%Y" "${OUT_FILE}")"
 if [ "${OLD_MODIFY}" -ne "${NEW_MODIFY}" ]
 then
     echo "File modified. Reencrypting..."
-    gpg --encrypt --sign --yes --output "$IN_FILE" "$OUT_FILE"
+    gpg --encrypt --sign --yes --output "${IN_FILE}" "${OUT_FILE}"
 else
     echo "File not modified"
 fi
