0ea060660301ae17c90805539bad78feaac975eb
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:33 +0400

    Change pulse device

--- stream-webcam.sh
+++ stream-webcam.sh
@@ -4,6 +4,6 @@
 # https://wiki.videolan.org/Documentation:Streaming_HowTo/Advanced_Streaming_Using_the_Command_Line
 ffmpeg \
     -f video4linux2 -video_size 1280x720 -framerate 30 -thread_queue_size 1024 -input_format mjpeg -i /dev/video0 \
-    -f pulse -thread_queue_size 1024 -i default \
+    -f pulse -thread_queue_size 1024 -i alsa_input.pci-0000_05_00.6.analog-stereo \
     -f mpegts -vcodec h264_nvenc -acodec aac - \
     | cvlc -I dummy - --sout '#rtp{sdp=rtsp://0.0.0.0:5000/,proto=udp}' --sout-rtsp-user user --sout-rtsp-pwd 111111
