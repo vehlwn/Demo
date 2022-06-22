b28cd93417c5c027470573e4c4b9038bf95528f1
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:30 +0400

    Strengthen version constraints

--- pip-upgrade.sh
+++ pip-upgrade.sh
@@ -2,6 +2,6 @@
 
 tmpfile=$(mktemp)
 pip3 freeze > "$tmpfile"
-sed 's/=.*$//' -i "$tmpfile"
-pip3 install -r "$tmpfile" --upgrade
+sed 's/==/>=/' -i "$tmpfile"
+pip3 install -r "$tmpfile" --upgrade --user
 rm "$tmpfile"
