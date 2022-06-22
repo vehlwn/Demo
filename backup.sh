97b982ddcef5cfb7be692a3668c219ec11bb60e7
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:11 +0400

    Remove time zone

--- backup.sh
+++ backup.sh
@@ -25,7 +25,7 @@ readonly SOURCE_DIR="/mnt/e/1/0 /home/vehlwn"
 readonly BACKUP_DIR="/mnt/cryptbackup/backup"
 
 readonly OPTS="--archive --acls --xattrs --human-readable --partial --one-file-system --delete --progress --verbose"
-readonly DATETIME="$(date --iso-8601=seconds)"
+readonly DATETIME="$(date '+%Y-%m-%dT%H:%M:%S')"
 readonly BACKUP_PATH="${BACKUP_DIR}/${DATETIME}"
 readonly LATEST_LINK="${BACKUP_DIR}/latest"
 
