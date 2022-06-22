dff73378bc83375e7743100e1bd9782870f442e0
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:36 +0400

    Use mp4

--- record-x11.sh
+++ record-x11.sh
@@ -2,4 +2,4 @@
 ffmpeg -y \
     -f x11grab -video_size 1920x1080 -framerate 60 -thread_queue_size 1024 -i :0 \
     -f pulse -thread_queue_size 1024 -i alsa_output.pci-0000_05_00.6.analog-stereo.monitor \
-    -codec:v h264_nvenc -codec:a aac -b:v 8M output.mkv
+    -codec:v h264_nvenc -codec:a aac -b:v 8M output.mp4
