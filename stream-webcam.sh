faaaf9372e5ddebb6214ac0d9520bff8afd76d98
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:47 +0400

    Change nvenc to vaapi

--- stream-webcam.sh
+++ stream-webcam.sh
@@ -4,9 +4,15 @@ set -e
 # https://wiki.videolan.org/Documentation:Streaming_HowTo/Command_Line_Examples/
 # https://wiki.videolan.org/Documentation:Streaming_HowTo/Advanced_Streaming_Using_the_Command_Line
 ffmpeg \
+    -hide_banner \
+    -vaapi_device /dev/dri/renderD128 \
     -f video4linux2 -video_size 1280x720 -framerate 30 -thread_queue_size 1024 -input_format mjpeg -i /dev/video0 \
     -f pulse -thread_queue_size 1024 -i alsa_input.pci-0000_05_00.6.analog-stereo \
-    -f mpegts -vcodec h264_nvenc -acodec aac \
-    -filter_complex "drawtext=text='%{localtime\\:%F %T} n=%{frame_num}' :box=1 :boxcolor=black@0.2 :boxborderw=2 :fontcolor=white :fontsize=28 :x=10 :y=10" \
+    -f mpegts -vcodec h264_vaapi -acodec aac \
+    -filter_complex "
+        drawtext=text='%{localtime\\:%F %T} n=%{frame_num}' :box=1 :boxcolor=black@0.2 :boxborderw=2 :fontcolor=white :fontsize=28 :x=10 :y=10
+        ,format=nv12
+        ,hwupload
+        " \
     - \
     | cvlc -I dummy - --sout '#rtp{sdp=rtsp://0.0.0.0:5000/,proto=udp}' --sout-rtsp-user user --sout-rtsp-pwd 111111
