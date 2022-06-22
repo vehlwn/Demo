c23f6228f769584e370e46920e73445be591b586
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:39 +0400

    Restore thread_queue_size

--- record-x11.sh
+++ record-x11.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ffmpeg -y \
-    -f x11grab -video_size 1920x1080 -framerate 60 -i :0 \
-    -f pulse -i alsa_output.pci-0000_05_00.6.analog-stereo.monitor \
+    -f x11grab -video_size 1920x1080 -framerate 60 -thread_queue_size 1024 -i :0 \
+    -f pulse -thread_queue_size 1024 -i alsa_output.pci-0000_05_00.6.analog-stereo.monitor \
     -codec:v h264_nvenc -codec:a aac -b:v 8M output.mp4
