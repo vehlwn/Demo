3e17819b000e88ad1471502eb9fbcb733d50c9f5
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:31 +0400

    Fix RTP transport

--- stream-webcam.sh
+++ stream-webcam.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-~/ffmpeg \
+# https://wiki.videolan.org/Documentation:Streaming_HowTo/Command_Line_Examples/
+# https://wiki.videolan.org/Documentation:Streaming_HowTo/Advanced_Streaming_Using_the_Command_Line
+ffmpeg \
     -f video4linux2 -video_size 1280x720 -framerate 30 -thread_queue_size 1024 -input_format mjpeg -i /dev/video0 \
     -f pulse -thread_queue_size 1024 -i default \
     -f mpegts -vcodec h264_nvenc -acodec aac - \
-    | cvlc -I dummy - --sout '#rtp{sdp=rtsp://0.0.0.0:5000/,proto=tcp}' --sout-rtsp-user user --sout-rtsp-pwd 111111
+    | cvlc -I dummy - --sout '#rtp{sdp=rtsp://0.0.0.0:5000/,proto=udp}' --sout-rtsp-user user --sout-rtsp-pwd 111111
