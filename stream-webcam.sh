2342f338949d65b45a113fe34c249ca619c5bb7f
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 16:50:27 +0400

    Use mjpeg input format

--- stream-webcam.sh
+++ stream-webcam.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-~/ffmpeg -s 640x480 -framerate 30 -thread_queue_size 1024 -i /dev/video0 -f pulse -thread_queue_size 1024 -i default -vcodec h264_nvenc -acodec aac -f mpegts - | cvlc -vvv -I dummy - --sout '#rtp{sdp=rtsp://0.0.0.0:5000/,proto=tcp}' --sout-rtsp-user user --sout-rtsp-pwd 111111
+~/ffmpeg \
+    -f video4linux2 -video_size 1280x720 -framerate 30 -thread_queue_size 1024 -input_format mjpeg -i /dev/video0 \
+    -f pulse -thread_queue_size 1024 -i default \
+    -f mpegts -vcodec h264_nvenc -acodec aac - \
+    | cvlc -I dummy - --sout '#rtp{sdp=rtsp://0.0.0.0:5000/,proto=tcp}' --sout-rtsp-user user --sout-rtsp-pwd 111111
