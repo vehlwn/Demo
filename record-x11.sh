5c7ede13e2c27a17ddbbeca4bdca5384853173f6
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-10-18 16:15:49 +0400

    Increase video bitrate

--- record-x11.sh
+++ record-x11.sh
@@ -2,4 +2,4 @@
 ffmpeg -y \
     -f x11grab -video_size 1920x1080 -framerate 60 -thread_queue_size 1024 -probesize 20M -i :0 \
     -f pulse -thread_queue_size 1024 -i alsa_output.pci-0000_05_00.6.analog-stereo.monitor \
-    -codec:v h264_nvenc -codec:a aac -b:v 8M output.mp4
+    -codec:v h264_nvenc -b:v 10M -codec:a aac -b:a 128k output.mp4
