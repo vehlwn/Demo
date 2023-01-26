7ba5d42e8e40d33c256af58cc23db627fc271bdc
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-01-26 16:26:21 +0400

    Add grep alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias ip='command ip -color=auto'
 alias l='command ls -lah --color=auto --group-directories-first --classify'
 alias sudo='command sudo --validate && command sudo '
+alias grep='command grep --color=auto'
