fdcfddca845284a1f9d4d68a1680273276a8cc46
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-07-20 09:16:29 +0400

    Change thread_queue_size

--- stream-webcam.sh
+++ stream-webcam.sh
@@ -7,10 +7,10 @@ set -e
 ffmpeg \
     -hide_banner \
     -vaapi_device /dev/dri/renderD128 \
-    -f video4linux2 -video_size 1280x720 -framerate 30 -thread_queue_size 1024 -input_format mjpeg -i /dev/video0 \
-    -f pulse -thread_queue_size 1024 -i alsa_input.pci-0000_05_00.6.analog-stereo \
+    -f video4linux2 -video_size 1280x720 -framerate 30 -thread_queue_size 128 -input_format mjpeg -i /dev/video0 \
+    -f pulse -thread_queue_size 128 -i default \
     -f mpegts \
-    -vcodec h264_vaapi -b:v 5M -g 15 \
+    -vcodec h264_vaapi -b:v 5M -g 15 -flags +cgop \
     -acodec aac -b:a 128K \
     -filter_complex "
         drawtext=text='%{localtime\\:%F %T} n=%{frame_num}' :box=1 :boxcolor=black@0.2 :boxborderw=2 :fontcolor=white :fontsize=28 :x=10 :y=10
