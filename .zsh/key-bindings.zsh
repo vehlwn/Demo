3f2990dd1334a99b9cda8bc5062ea63cb1bc7c66
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-07-27 14:28:34 +0400

    Use terminfo

--- .zsh/key-bindings.zsh
+++ .zsh/key-bindings.zsh
@@ -1,23 +1,42 @@
 # emacs mode
 bindkey -e
 
+# create a zkbd compatible hash;
+# to add other keys to this hash, see: man 5 terminfo
+typeset -g -A key
+
+key[Home]="${terminfo[khome]}"
+key[End]="${terminfo[kend]}"
+key[Backspace]="${terminfo[kbs]}"
+key[Delete]="${terminfo[kdch1]}"
+key[Up]="${terminfo[kcuu1]}"
+key[Down]="${terminfo[kcud1]}"
+key[Left]="${terminfo[kcub1]}"
+key[Right]="${terminfo[kcuf1]}"
+key[Shift-Tab]="${terminfo[kcbt]}"
+
 # setup key accordingly:
-# home
-bindkey "^[[1~" beginning-of-line
-# end
-bindkey "^[[4~" end-of-line
-# delete
-bindkey "^[[3~" delete-char
-# left
-bindkey "^[[D" backward-char
-# right
-bindkey "^[[C" forward-char
-# shift+tab
-bindkey "^[[Z" reverse-menu-complete
+[[ -n "${key[Home]}"      ]] && bindkey -- "${key[Home]}"       beginning-of-line
+[[ -n "${key[End]}"       ]] && bindkey -- "${key[End]}"        end-of-line
+[[ -n "${key[Backspace]}" ]] && bindkey -- "${key[Backspace]}"  backward-delete-char
+[[ -n "${key[Delete]}"    ]] && bindkey -- "${key[Delete]}"     delete-char
+[[ -n "${key[Left]}"      ]] && bindkey -- "${key[Left]}"       backward-char
+[[ -n "${key[Right]}"     ]] && bindkey -- "${key[Right]}"      forward-char
+[[ -n "${key[Shift-Tab]}" ]] && bindkey -- "${key[Shift-Tab]}"  reverse-menu-complete
 # control+left
-bindkey '^[[1;5C' forward-word
+bindkey -- '^[[1;5C' forward-word
 # control+right
-bindkey '^[[1;5D' backward-word
+bindkey -- '^[[1;5D' backward-word
 # zsh-users/zsh-history-substring-search keys:
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+[[ -n "${key[Up]}" ]] && bindkey -- "${key[Up]}"  history-substring-search-up
+[[ -n "${key[Down]}" ]] && bindkey -- "${key[Down]}"  history-substring-search-down
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+    autoload -Uz add-zle-hook-widget
+    function zle_application_mode_start { echoti smkx }
+    function zle_application_mode_stop { echoti rmkx }
+    add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+    add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
