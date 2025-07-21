9478b109499be61bf4e26919411a7c6f978fde13
vehlwn <36479428+vehlwn@users.noreply.github.com>
2025-07-21 12:20:31 +0400

    Use full directory path

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -2,7 +2,7 @@
 function prompt_mytheme_setup() {
     local FG_COLOR="black"
     local BK_COLOR="#F48FB1"
-    PS1="%F{${FG_COLOR}}%K{${BK_COLOR}}%n@%m%k%f%(?, , %K{red}[%?]%k )%F{green}%B%~%b%f
+    PS1="%F{${FG_COLOR}}%K{${BK_COLOR}}%n@%m%k%f%(?, , %K{red}[%?]%k )%F{green}%B%/%b%f
 \$ "
 }
 prompt_themes+=( mytheme )
